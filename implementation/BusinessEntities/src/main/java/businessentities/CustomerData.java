cd84a4596b8caf831a8a588b4877e56baccc79d3
Bonajo <m.bonajo@fontys.nl>
2022-03-10 13:22:43 +0100

    feat: add ID to CustomerData

--- implementation/BusinessEntities/src/main/java/businessentities/CustomerData.java
+++ implementation/BusinessEntities/src/main/java/businessentities/CustomerData.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
  */
 public class CustomerData {
     
+    private final int ID;
     private final String firstName;
     private final String surName;
     private final LocalDate birthDate;
@@ -17,16 +18,18 @@ public class CustomerData {
      * Create new customer data object
      * 
      * Validates that the given data is correct
+     * @param id customer's ID
      * @param firstName customer's first name
      * @param surName customer's surname
-     * @param birthDate customer's birtdate
+     * @param birthDate customer's birthdate
      */
-    public CustomerData(String firstName, String surName, LocalDate birthDate) {
+    public CustomerData(int id, String firstName, String surName, LocalDate birthDate) {
+        this.ID = id;
         this.firstName = firstName;
         this.surName = surName;
         
         if(birthDate.isAfter(LocalDate.now())){
-            throw new IllegalArgumentException("Birthday cannot be in the future");
+            throw new IllegalArgumentException("Birthdate cannot be in the future");
         }
         this.birthDate = birthDate;
     }
