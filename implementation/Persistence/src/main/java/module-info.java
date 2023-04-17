bdcf689b4590a5725f6ea23b716331dd9e34f05b
Richard van den Ham <R.vandenham@fontys.nl>
2023-04-17 14:05:03 +0200

    feat: example of dealing with DB properties in layered architecture

--- implementation/Persistence/src/main/java/module-info.java
+++ implementation/Persistence/src/main/java/module-info.java
@@ -1,4 +1,10 @@
 module persistence_module { 
     requires datarecords_module;
+    
+    requires java.logging;
+    requires java.sql;
+    requires org.postgresql.jdbc;
+    requires java.naming;
+    
     exports persistence;
 }
