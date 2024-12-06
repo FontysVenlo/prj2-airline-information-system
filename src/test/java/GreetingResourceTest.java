f8b58a62214ea88556930dcb8ed5a1101e5c5f3c
Lennart Tange <1272512+lenntt@users.noreply.github.com>
2024-12-06 08:36:04 +0100

    experiment: quarkus tutorial

---
+++ src/test/java/GreetingResourceTest.java
@@ -0,0 +1,33 @@
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/fruits")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(is("hello " + uuid));
+    }
+}
