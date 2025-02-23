9525b55be0cf714c5a245149d25e3db374e723d4
Richard van den Ham <R.vandenham@fontys.nl>
2025-02-23 15:58:36 +0100

    feat: demo JPMS compliant

---
+++ backend/assembler/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * Module assembler_module.
+ */
+module assembler_module{
+
+    requires persistence_module;
+    requires businesslogic_module;
+    requires datarecords_module;
+    requires restapi_module;
+    
+    requires java.logging;
+
+    
+}
---
+++ backend/restapi/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * Module restapi_module.
+ */
+module restapi_module {
+
+    requires businesslogic_module;
+    requires datarecords_module;
+
+    requires java.logging;
+    requires java.base;
+        
+    requires io.javalin;
+    requires com.fasterxml.jackson.databind;
+    
+    exports io.github.fontysvenlo.ais.restapi;
+}
