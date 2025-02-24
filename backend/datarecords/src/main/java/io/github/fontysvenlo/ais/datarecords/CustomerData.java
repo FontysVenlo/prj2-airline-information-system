335b7b2bc20391c1b878bf378b4b373e53988b64
Richard van den Ham <R.vandenham@fontys.nl>
2025-02-24 21:36:42 +0100

    feat: ID changed to Integer, example validator

--- backend/datarecords/src/main/java/io/github/fontysvenlo/ais/datarecords/CustomerData.java
+++ backend/datarecords/src/main/java/io/github/fontysvenlo/ais/datarecords/CustomerData.java
@@ -11,4 +11,19 @@ import java.time.LocalDate;
  * @param lastName the last name of the customer
  * @param dateOfBirth the date of birth of the customer
  */
-public record CustomerData(int id, String firstName, String lastName, LocalDate dateOfBirth) {}
+public record CustomerData(Integer id, String firstName, String lastName, LocalDate dateOfBirth) {
+
+    /**
+     * Validator.
+     * @param id see record JavaDoc
+     * @param firstName see record JavaDoc
+     * @param lastName see record JavaDoc
+     * @param dateOfBirth see record JavaDoc
+     * @throws IllegalArgumentException if first name is empty
+     */
+    public CustomerData {
+        if (firstName.isBlank()) {
+            throw new IllegalArgumentException("Customer first name is not allowed to be empty");
+        }
+    }
+}
