f6d12302216531d43038886cfcb7354ccdeef038
Bonajo <m.bonajo@fontys.nl>
2025-02-24 21:16:46 +0100

    feat: add frontend

---
+++ frontend/src/routes/customers/[slug]/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {onMount} from "svelte";
+    import {api} from "$lib/api.js";
+    import { page } from '$app/state';
+
+    let customer = $state({});
+
+    onMount(() => {
+        load()
+    });
+
+    const load = async () => {
+        try{
+            customer = await api.one("customers", page.params.slug);
+        }catch (e) {
+            // Handle the error
+            console.log(e);
+        }
+    }
+</script>
+
+<h1 class="text-4xl">Customer</h1>
+
+Do something with the customer data here.
